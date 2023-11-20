92c30c56766f1e1fc932dc1788b0f883b17925e3
Vix <148944804+ttfe21@users.noreply.github.com>
2023-11-20 10:41:15 +0700

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -16,19 +16,19 @@ int main() {
 		else if (userCommand.compare("2") == 0) {
 			system("cls");
 			int res = wg->addToPracticeList("PracticeWords.txt");
-			if (res == 1) std::cout << "Added completely.\n";
-			else if (res == 2) std::cout << "existing word.\n";
-			else if (res == 3) std::cout << "NO existing word.\n";
-			else std::cout << "Failed adding.\n";
+			if (res == 1) std::cout << "Word added.\n";
+			else if (res == 2) std::cout << "Word already added.\n";
+			else if (res == 3) std::cout << "Sorry, that word isn\'t in our dictionary yet.\n";
+			else std::cout << "Adding word failed.\n";
 			std::cout << "-------------------------------------------------------------\n";
 		}
 		else if (userCommand.compare("3") == 0) {
 			system("cls");
 			int res = wg->removeFromPracticeList("PracticeWords.txt");
-			if (res == 1) std::cout << "Remove completely.\n";
-			else if (res == 2) std::cout << "NO existing word.\n";
-			else if (res == 0) std::cout << "Empty practice list.\n";
-			else std::cout << "Failed removing.\n";
+			if (res == 1) std::cout << "Word removed.\n";
+			else if (res == 2) std::cout << "Sorry, that word isn\'t in our dictionary yet.\n";
+			else if (res == 0) std::cout << "Practice list is empty.\n";
+			else std::cout << "Removing word failed.\n";
 			std::cout << "-------------------------------------------------------------\n";
 		}
 		else if (userCommand.compare("4") == 0) {
@@ -44,15 +44,15 @@ int main() {
 			wg->playHidingWordsGame();
 		}
 		else if (userCommand.compare("h") == 0 || userCommand.compare("?") == 0) {
-			std::cout << "1 \t-> Look Up a Word\n";
-			std::cout << "2 \t-> Add words for Practice\n";
-			std::cout << "3 \t-> Remove words from Practice List\n";
-			std::cout << "4 \t-> Play Quiz Game\n";
-			std::cout << "5 \t-> Play Missing Letter Game\n";
-			std::cout << "6 \t-> Play Hiding Words Game\n";
-			std::cout << "q \t-> Quit\n";
-			std::cout << "f \t-> Flush the Screen\n";
-			std::cout << "h or ? \t-> See the instruction\n";
+			std::cout << "   1 \t\t-> Look Up a Word\n";
+			std::cout << "   2 \t\t-> Add words for Practice\n";
+			std::cout << "   3 \t\t-> Remove words from Practice List\n";
+			std::cout << "   4 \t\t-> Play Quiz Game\n";
+			std::cout << "   5 \t\t-> Play Missing Letter Game\n";
+			std::cout << "   6 \t\t-> Play Hiding Words Game\n";
+			std::cout << "   q \t\t-> Quit\n";
+			std::cout << "   f \t\t-> Flush the Screen\n";
+			std::cout << "   h or ? \t-> See the instruction\n";
 			std::cout << "-------------------------------------------------------------\n";
 		}
 		else if (userCommand.compare("f") == 0) {
@@ -65,7 +65,7 @@ int main() {
 			ConsoleEffect::foregroundYellowB("\'?\'");
 			std::cout << " for help\n";
 		}
-		ConsoleEffect::foregroundGreenB("\\main_run>");
+		ConsoleEffect::foregroundGreenB("\\main_run> ");
 		std::getline(std::cin, userCommand);
 	}
 
